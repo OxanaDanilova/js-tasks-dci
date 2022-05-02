9df6eecbb43f69ee527ee769ec4e7b260c4a7e8c
Oxana Danilova <oxanadanilova11@gmail.com>
2022-05-02 11:23:36 +0200

    feat: add tasks 28042022

--- apr/28042022/tasks.js
+++ apr/28042022/tasks.js
@@ -26,6 +26,9 @@ console.log(humanize(302)); //"302nd"
 // console.log(alphabetize_string("United States"));
 // //"SUadeeinsttt"
 
+const alphabetize_string = (str) => str.split("").sort().join("");
+console.log(alphabetize_string("United States"));
+
 // Write a JavaScript function to sort the following array of objects by title value.
 var library = [
   { author: "Bill Gates", title: "The Road Ahead", libraryID: 1254 },
